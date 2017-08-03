b65670117e547f6cf3f0838de3893df7304d47fa
Martynas Bardauskas <martynasb@wix.com>
2017-08-03 11:39:37 +0300

    added new note activity

---
+++ app/src/main/java/com/example/martynasb/paranoidnotes/ParanoidNotes.java
@@ -0,0 +1,11 @@
+package com.example.martynasb.paranoidnotes;
+
+
+import android.app.Application;
+
+public class ParanoidNotes extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
