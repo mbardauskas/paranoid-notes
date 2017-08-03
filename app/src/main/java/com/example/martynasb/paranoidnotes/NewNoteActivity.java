786e1c84f01596eb89f5bd61431a42a865670c2d
Martynas Bardauskas <martynasb@wix.com>
2017-08-03 15:04:45 +0300

    using title and body for note item; adding to notelist on save

--- app/src/main/java/com/example/martynasb/paranoidnotes/NewNoteActivity.java
+++ app/src/main/java/com/example/martynasb/paranoidnotes/NewNoteActivity.java
@@ -3,10 +3,10 @@ package com.example.martynasb.paranoidnotes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class NewNoteActivity extends AppCompatActivity {
@@ -22,7 +22,16 @@ public class NewNoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(NewNoteActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+                final EditText noteInput = (EditText) findViewById(R.id.new_note_input);
+                final EditText noteTitleInput = (EditText) findViewById(R.id.new_note_title_input);
+                final NoteService noteService = ((ParanoidNotes)getApplication()).getNoteService();
+
+                noteService.addNote(new NoteItem(
+                        noteTitleInput.getText().toString(),
+                        noteInput.getText().toString()
+                ));
+
+                Toast.makeText(NewNoteActivity.this, noteInput.getText(), Toast.LENGTH_SHORT).show();
             }
         });
     }
